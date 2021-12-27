11a0e376a4e3c8f32da44979833d640a287fc94c
shubhamborke <shubham.borke@gmail.com>
2021-12-28 01:56:41 +0530

    Modify site styles

--- src/components/cards.js
+++ src/components/cards.js
@@ -6,7 +6,7 @@ const Cards = (props) => {
   return (
     <div className="box-model">
       <h2 className="head">{props.elem.title}</h2>
-      <img src={props.elem.image}></img>
+      <Link to={`/CartPage`}><img onClick={Click} src={props.elem.image}></img></Link>
       <p className="overflow">{props.elem.description}</p>
       <div className="bottom">
         <p>Price: {props.elem.price} $</p>
--- src/components/cartPage.js
+++ src/components/cartPage.js
@@ -1,10 +1,38 @@
-const CartPage = (props) => {
-    console.log(props.state.id)
-    return (
-        <div>
-            <h1>welcome to card page</h1>
-        </div>
-    )
-}
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export default CartPage
+const CartPage = (props) => {
+  const [count, setcount] = useState(1);
+  return (
+    <div className="cartcontainerMain">
+      <h1 className="cartHead">Add To Cart</h1>
+      <div className="cartContainer">
+        <div className="cartBox1">
+          <img src={props.state.image}></img>
+          <div className="quantity">
+            <p className="quntitygap">Quntity</p>
+            <i className="fas quntitygap fa-minus-circle"></i>
+            <p className="quntitygap">{count}</p>
+            <i className="fas quntitygap fa-plus-circle"></i>
+          </div>
+        </div>
+        <div className="cartBox2">
+          <p>{props.state.description}</p>
+          <div className="pricing">
+            <h3>Price: {props.state.price} $</h3>
+            <h3>
+              Rating: {props.state.rating.rate}
+              <i className="far fa-star"></i>
+            </h3>
+          </div>
+          <button className="cartbutton1">Buy Now</button>
+          <button className="cartbutton2">
+            <Link to="/">Shop Again</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
